f463367bd973139a3b8cdda0c4a443e0cea14596
Aditya Rajput <ishanandaditya@gmail.com>
2026-07-28 23:44:03 +0530

    Bubble Sort Solved

--- Learn Important Sorting Techniques/Sorting I/BubbleSort.java
+++ Learn Important Sorting Techniques/Sorting I/BubbleSort.java
@@ -1,5 +1,20 @@
 class BubbleSort{
     public static void main(String[] args){
-        
+        int[] arr = {23, 5, 12, 3, 19};
+        for(int i = 0; i<arr.length-1; i++){
+            for(int j = 0; j<arr.length-i-1; j++){
+                //compare
+                if(arr[j]>arr[j+1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        //print array
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
