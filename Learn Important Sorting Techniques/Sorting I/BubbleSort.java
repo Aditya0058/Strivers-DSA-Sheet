1479c8d8bb419ee868d7aa36ed0ff2f30fc83b22
Aditya Rajput <ishanandaditya@gmail.com>
2026-07-28 23:50:10 +0530

    Optimized the Bubble sort algo

--- Learn Important Sorting Techniques/Sorting I/BubbleSort.java
+++ Learn Important Sorting Techniques/Sorting I/BubbleSort.java
@@ -2,14 +2,21 @@ class BubbleSort{
     public static void main(String[] args){
         int[] arr = {23, 5, 12, 3, 19};
         for(int i = 0; i<arr.length-1; i++){
+            int didSwap = 0;
             for(int j = 0; j<arr.length-i-1; j++){
+                
                 //compare
                 if(arr[j]>arr[j+1]){
                     // swap
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    didSwap = 1;
                 }
+
+            }
+            if(didSwap == 0){
+                break;
             }
         }
         //print array
