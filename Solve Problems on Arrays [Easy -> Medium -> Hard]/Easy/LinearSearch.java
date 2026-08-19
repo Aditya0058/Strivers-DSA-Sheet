9f6c41cc4012bb8c03816bb2b7ae01e3c65f7bd5
Aditya Rajput <ishanandaditya@gmail.com>
2026-08-19 19:54:14 +0530

    Solved Linear Search

---
+++ Solve Problems on Arrays [Easy -> Medium -> Hard]/Easy/LinearSearch.java
@@ -0,0 +1,11 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2,3, 4};
+        int find = 3;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == find){
+                System.out.print(i);
+            }
+        }
+    }
+}
